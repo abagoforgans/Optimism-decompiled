64c28d92a41ca17859b46eb017e894b052d345d2
bot <abagoforgans@users.noreply.github.com>
2022-03-19 13:23:31 +0000

    autocommit

---
+++ contracts/0x061605c4Ad8825E3B6731875B409D77f19FD75C9.sol
@@ -0,0 +1,149 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+uint8 stor0;
+uint8 stor0; offset 8
+uint256 stor0; offset 8
+address owner;
+address WETHAddress;
+
+function owner() {
+    return owner
+}
+
+function WETH() {
+    return WETHAddress
+}
+
+function _fallback() payable {
+  stop
+}
+
+function renounceOwnership() {
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    emit OwnershipTransferred(owner, 0);
+    owner = 0
+}
+
+function setWETHAddress(address arg1) {
+    require calldata.size - 4 >= 32
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    WETHAddress = arg1
+}
+
+function sub_3847d7eb(?) {
+    require calldata.size - 4 >= 32
+    require ext_code.size(WETHAddress)
+    call WETHAddress.withdraw(uint256 rg1) with:
+         gas gas_remaining wei
+        args arg1
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+}
+
+function rescue(uint256 arg1) {
+    require calldata.size - 4 >= 32
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    require ext_code.size(WETHAddress)
+    call WETHAddress.transfer(address rg1, uint256 rg2) with:
+         gas gas_remaining wei
+        args owner, arg1
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+    require return_data.size >= 32
+}
+
+function transferOwnership(address arg1) {
+    require calldata.size - 4 >= 32
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    38,
+                    0xfe4f776e61626c653a206e6577206f776e657220697320746865207a65726f20616464726573,
+                    mem[202 len 26]
+    emit OwnershipTransferred(owner, arg1);
+    owner = arg1
+}
+
+function initialize() {
+    if uint8(stor0.field_8):
+        owner = msg.sender
+        emit OwnershipTransferred(0, msg.sender);
+    else:
+        if ext_code.size(this.address):
+            if uint8(stor0.field_0):
+                revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                            32,
+                            46,
+                            0x73496e697469616c697a61626c653a20636f6e747261637420697320616c726561647920696e697469616c697a65,
+                            mem[210 len 18]
+        if uint8(stor0.field_8):
+            owner = msg.sender
+            emit OwnershipTransferred(0, msg.sender);
+        else:
+            uint8(stor0.field_0) = 1
+            uint8(stor0.field_8) = 1
+            Mask(248, 0, stor0.field_8) = 0
+            if ext_code.size(this.address):
+                if uint8(stor0.field_0):
+                    revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                                32,
+                                46,
+                                0x73496e697469616c697a61626c653a20636f6e747261637420697320616c726561647920696e697469616c697a65,
+                                mem[210 len 18]
+            if uint8(stor0.field_8):
+                owner = msg.sender
+                emit OwnershipTransferred(0, msg.sender);
+            else:
+                uint8(stor0.field_0) = 1
+                uint8(stor0.field_8) = 1
+                Mask(248, 0, stor0.field_8) = 0
+                if ext_code.size(this.address):
+                    if uint8(stor0.field_0):
+                        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                                    32,
+                                    46,
+                                    0x73496e697469616c697a61626c653a20636f6e747261637420697320616c726561647920696e697469616c697a65,
+                                    mem[210 len 18]
+                if uint8(stor0.field_8):
+                    owner = msg.sender
+                    emit OwnershipTransferred(0, msg.sender);
+                else:
+                    uint8(stor0.field_0) = 1
+                    uint8(stor0.field_8) = 1
+                    Mask(248, 0, stor0.field_8) = 0
+                    uint8(stor0.field_8) = 0
+                    if ext_code.size(this.address):
+                        if uint8(stor0.field_0):
+                            revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                                        32,
+                                        46,
+                                        0x73496e697469616c697a61626c653a20636f6e747261637420697320616c726561647920696e697469616c697a65,
+                                        mem[210 len 18]
+                    if uint8(stor0.field_8):
+                        owner = msg.sender
+                        emit OwnershipTransferred(0, msg.sender);
+                    else:
+                        uint8(stor0.field_0) = 1
+                        uint8(stor0.field_8) = 1
+                        Mask(248, 0, stor0.field_8) = 0
+                        owner = msg.sender
+                        emit OwnershipTransferred(0, msg.sender);
+                        uint8(stor0.field_8) = 0
+                        uint8(stor0.field_8) = 0
+                        uint8(stor0.field_8) = 0
+}
+
+
+
+}
