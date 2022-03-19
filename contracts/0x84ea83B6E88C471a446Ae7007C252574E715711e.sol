22e5cd8fd15352e647233648dcdca0860bca5a2c
bot <abagoforgans@users.noreply.github.com>
2022-03-19 10:45:25 +0000

    autocommit

---
+++ contracts/0x84ea83B6E88C471a446Ae7007C252574E715711e.sol
@@ -0,0 +1,64 @@
+contract main {
+
+
+
+
+// =====================  Runtime code  =====================
+
+
+address owner;
+address WETHAddress;
+
+function owner() payable {
+    return owner
+}
+
+function WETH() payable {
+    return WETHAddress
+}
+
+function _fallback() payable {
+    revert
+}
+
+function renounceOwnership() payable {
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    emit OwnershipTransferred(owner, 0);
+    owner = 0
+}
+
+function setWETHAddress(address arg1) payable {
+    require calldata.size - 4 >= 32
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    WETHAddress = arg1
+}
+
+function sub_3847d7eb(?) payable {
+    require calldata.size - 4 >= 32
+    require ext_code.size(WETHAddress)
+    call WETHAddress.withdraw(uint256 rg1) with:
+         gas gas_remaining wei
+        args arg1
+    if not ext_call.success:
+        revert with ext_call.return_data[0 len return_data.size]
+}
+
+function transferOwnership(address arg1) payable {
+    require calldata.size - 4 >= 32
+    if owner != msg.sender:
+        revert with 0, 'Ownable: caller is not the owner'
+    if not arg1:
+        revert with 0x8c379a000000000000000000000000000000000000000000000000000000000, 
+                    32,
+                    38,
+                    0xfe4f776e61626c653a206e6577206f776e657220697320746865207a65726f20616464726573,
+                    mem[202 len 26]
+    emit OwnershipTransferred(owner, arg1);
+    owner = arg1
+}
+
+
+
+}
